fb1811b2d4387afb83d21fb7af6e30e4e19e726f
DANH LE <letranhuudanh0030@gmail.com>
2022-02-08 12:40:32 +0700

    fix ssl

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -28,7 +27,7 @@ class AppServiceProvider extends ServiceProvider
         if (env('APP_ENV') === 'production') {
             $this->app['request']->server->set('HTTPS','on'); // this line
 
-            URL::forceSchema('https');
+            \URL::forceScheme('https');
         }
     }
 }
